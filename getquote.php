9edb04e907fba79ff8eb7114459d4b4cd7a489af
John Cheng <devnull@johncheng.com>
2015-03-07 08:33:13 -0800

    spacing

--- getquote.php
+++ getquote.php
@@ -1,13 +1,11 @@
 <?php
 require_once 'rss_php.php';    
+$rss = new rss_php;
+$rss->load('http://www.quotedb.com/quote/quote.php?action=random_quote_rss');
+$rssitems = $rss->getItems();
 
-        $rss = new rss_php;
-        $rss->load('http://www.quotedb.com/quote/quote.php?action=random_quote_rss');
-        $rssitems = $rss->getItems();
-
-        if ($rssitems) {
-                // print_r($rssitems);
-                echo $rssitems[0]['description'].' :: '.$rssitems[0]['title']."\n";
-        }
-   
+if ($rssitems) {
+    // print_r($rssitems);
+    echo $rssitems[0]['description'].' :: '.$rssitems[0]['title']."\n";
+}
 ?>
