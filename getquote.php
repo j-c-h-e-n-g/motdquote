d8f4c459c7b66d925da252ed17df416f830800be
John Cheng <devnull@johncheng.com>
2015-03-07 08:29:36 -0800

    1st commit

---
+++ getquote.php
@@ -0,0 +1,13 @@
+<?php
+require_once 'rss_php.php';    
+
+        $rss = new rss_php;
+        $rss->load('http://www.quotedb.com/quote/quote.php?action=random_quote_rss');
+        $rssitems = $rss->getItems();
+
+        if ($rssitems) {
+                // print_r($rssitems);
+                echo $rssitems[0]['description'].' :: '.$rssitems[0]['title']."\n";
+        }
+   
+?>
